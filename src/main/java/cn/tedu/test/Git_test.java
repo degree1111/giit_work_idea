b47460955e4d2def59c7d77bd0265b19119f489f
degree <2427641471@qq.com>
2023-03-22 15:19:02 +0800

    01 quanbu_commit

--- src/main/java/cn/tedu/test/Git_test.java
+++ src/main/java/cn/tedu/test/Git_test.java
@@ -5,5 +5,6 @@ public class Git_test {
     public static void main(String[] args) {
         System.out.println("测试一下Git");
         System.out.println("第一次修改");
+        System.out.println("这里是hot-fix");
     }
 }
