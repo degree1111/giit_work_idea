d1f017379e93431cc49c4f6a56ab5055690e64de
degree <2427641471@qq.com>
2023-03-22 17:42:12 +0800

    clone后，测试pulll

--- src/main/java/cn/tedu/test/Git_test.java
+++ src/main/java/cn/tedu/test/Git_test.java
@@ -3,7 +3,7 @@ package cn.tedu.test;
 public class Git_test {
 
     public static void main(String[] args) {
-        System.out.println("清空，然后测试pull");
+        System.out.println("clone后清空，然后测试pull");
 
 
     }
