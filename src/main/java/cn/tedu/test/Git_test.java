aa4188add669907735c4a3bbff2f038869dd94b9
degree <2427641471@qq.com>
2023-03-22 15:29:37 +0800

    ct hot-fix01 commit

--- src/main/java/cn/tedu/test/Git_test.java
+++ src/main/java/cn/tedu/test/Git_test.java
@@ -4,7 +4,7 @@ public class Git_test {
 
     public static void main(String[] args) {
         System.out.println("测试一下Git");
-        System.out.println("第一次修改");
+        System.out.println("第一次修改，这里是hot-fix");
         System.out.println("重复：这里是hot-fix");
         System.out.println("这里是hot-fix的第二次尝试01");
     }
